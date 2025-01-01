dcb4214815b477ca4fa582410c5ee41b7385445b
Berkkan Rençber <berkkanrencber@gmail.com>
2025-01-01 14:53:53 +0300

    Project was updated. Navbar and Hero componet's design was finished.

---
+++ portfolio/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    personal: {
+      name: "Berkkan Rençber",
+      title: "Software Developer",
+      subtitle: "Creative thinker",
+      mainTitle1: "Creative thinker",
+      mainTitle2: "Minimalism lover",
+      about:
+        "I am Berkkan Rençber, a software developer with a strong passion for programming. I have experience in various programming languages and technologies, including JavaScript, React, Java, and more.",
+      location: "Kadıköy, Istanbul",
+      email: "berkkanrencber@gmail.com",
+      phone: "+90 531 523 28 13",
+      github: "https://github.com/berkkanrencber",
+      linkedin: "https://www.linkedin.com/in/berkkan-ren%C3%A7ber-906a1618b/",
+      image: "../src/assets/cv_photo.jpg",
+    },
+    skills: ["JavaScript", "React", "Node.js"],
+    profile: {
+      dob: "08.01.2001",
+      location: "Kadıköy, Istanbul",
+      educationStatus: "Bachelor's in Computer Science",
+      preferredRole: "Frontend Developer",
+      aboutMe:
+        "I am a highly motivated software developer passionate about creating scalable and user-friendly solutions.",
+      education: [
+        {
+          degree: "Computer Science Engineering",
+          school: "Marmara University",
+          year: "2019-2024",
+          gpa: "3.61/4",
+        },
+        {
+          degree: "High School Education",
+          school: "Samsun Final Anatolian High School",
+          year: "2015-2019",
+          gpa: "96.78/100",
+        },
+      ],
+      languages: ["Turkish (Native)", "English (Professional)", "German (Elementary)"],
+    },
+    projects: [
+      {
+        name: "MovieApp",
+        description:
+          "An Android application for viewing and managing favorite movies.",
+        techStack: ["Kotlin", "Retrofit", "ROOM"],
+        github: "https://github.com/berkkanrencber/MovieApp",
+        image: "../src/assets/movieapp.png",
+      },
+      {
+        name: "Atech Software Website",
+        description:
+          "A website built with Angular, Node.js, and MSSQL.",
+        techStack: ["Angular", "Node.js", "MSSQL"],
+        github: "https://www.atechyazilim.com",
+        image: "../src/assets/atech.png",
+        site: "https://www.atechyazilim.com",
+      },
+      {
+        name: "Journey",
+        description:
+          "A web-based travel app created with React and Tailwind CSS.",
+        techStack: ["React", "Tailwind CSS", "API"],
+        github: "https://github.com/berkkanrencber/Journey",
+        image: "../src/assets/project_wp.png",
+      },
+    ],
+  };
+  
+  export default data;
+  
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          purple: "#4731D3",
+          light_purple: "#B7AAFF",
+          gray: "#6B7280",
+          hero_title_light: "#AEBCCF",
+          hero_title_dark: "#1F2937",
+          text_gray: "#6B7280",
+        },
+        secondary: {
+        },
+        background: {
+          light: "#ffffff",
+          dark: "#252128",
+          logo_dark: "#4731D3",
+          logo_light: "#EEEBFF"
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
-}
-
+};
